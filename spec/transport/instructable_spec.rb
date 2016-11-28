775177575bea9ca97cda74b09873608f14b72fb0
John Lawrence <johnonrails@gmail.com>
2016-11-28 12:32:06 -0800

    Add support for multiple commands

--- spec/transport/instructable_spec.rb
+++ spec/transport/instructable_spec.rb
@@ -5,6 +5,10 @@ class InstructableClass
     called.push(:move)
   end
 
+  def speak
+    called.push(:speak)
+  end
+
   def called
     @called ||= []
   end
@@ -24,6 +28,14 @@ describe InstructableClass do
       it 'should not raise an error' do
         obj = described_class.new
         expect(-> { obj.listen_up!('POOP') }).not_to raise_error
+      end
+    end
+
+    context 'when message contains more than one an uppercase command matching a public method name' do
+      it 'causes that method to be called' do
+        obj = described_class.new
+        obj.listen_up!('MOVE SPEAK')
+        expect(obj.called).to eq [:move, :speak]
       end
     end
   end
--- transport/instructable/interpreter.rb
+++ transport/instructable/interpreter.rb
@@ -1,8 +1,22 @@
 module Transport
   module Instructable
     def listen_up!(message)
-      if public_methods.include? message.downcase.to_sym
-        send message.downcase
+      while token = eat_token(message)
+        if public_methods.include? token.downcase.to_sym
+          send token.downcase
+        end
+      end
+    end
+
+    private
+
+    def eat_token(message)
+      puts "message: '#{message}'"
+      matches = message.match /\A[^\s]+/
+      if matches
+        message.sub! matches[0], ''
+        message.sub! /\A\s/, ''
+        return matches[0]
       end
     end
   end
