c49ed1d92b83655ca9660ea68114c3460234277f
John Lawrence <johnonrails@gmail.com>
2016-11-29 22:33:37 -0800

    Add support for numericality

--- spec/transport/position2d_spec.rb
+++ spec/transport/position2d_spec.rb
@@ -10,5 +10,16 @@ describe Transport::Position2D do
         expect(position[1]).to eq y
       end
     end
+
+    context 'when x and y are strings that can be cast into integers' do
+      let(:x) { '5' }
+      let(:y) { '7' }
+
+      it 'returns a Position2D with components x.to_i and y.to_i' do
+        position = described_class[x, y]
+        expect(position[0]).to eq x.to_i
+        expect(position[1]).to eq y.to_i
+      end
+    end
   end
 end
--- transport/position2d.rb
+++ transport/position2d.rb
@@ -2,5 +2,8 @@ require 'matrix'
 
 module Transport
   class Position2D < Vector
+    def self.[](*args)
+      super(*args.map { |a| a.to_i })
+    end
   end
 end
