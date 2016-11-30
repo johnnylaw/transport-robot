01451ecc21b253c392f87145d9eaf38c0bc073b3
John Lawrence <johnonrails@gmail.com>
2016-11-30 11:57:02 -0800

    Test direction can be set by name

--- spec/functional/cardinal_directions_spec.rb
+++ spec/functional/cardinal_directions_spec.rb
@@ -18,6 +18,47 @@ describe Transport::CardinalDirections do
     end
 
     context 'when name is NORTH' do
+      let(:name) { 'NORTH' }
+
+      describe '#to_s and #coordinates' do
+        it 'indicates north' do
+          expect(directions.to_s).to eq 'NORTH'
+          expect(directions.coordinates).to eq [0, 1]
+        end
+      end
+    end
+
+    context 'when name is EAST' do
+      let(:name) { 'EAST' }
+
+      describe '#to_s and #coordinates' do
+        it 'indicates east' do
+          expect(directions.to_s).to eq 'EAST'
+          expect(directions.coordinates).to eq [1, 0]
+        end
+      end
+    end
+
+    context 'when name is SOUTH' do
+      let(:name) { 'SOUTH' }
+
+      describe '#to_s and #coordinates' do
+        it 'indicates south' do
+          expect(directions.to_s).to eq 'SOUTH'
+          expect(directions.coordinates).to eq [0, -1]
+        end
+      end
+    end
+
+    context 'when name is WEST' do
+      let(:name) { 'WEST' }
+
+      describe '#to_s and #coordinates' do
+        it 'indicates west' do
+          expect(directions.to_s).to eq 'WEST'
+          expect(directions.coordinates).to eq [-1, 0]
+        end
+      end
     end
   end
 end
--- transport/cardinal_directions.rb
+++ transport/cardinal_directions.rb
@@ -4,12 +4,18 @@ module Transport
   class CardinalDirections < Directions
     def direction_options
       @direction_options ||= [
+        Direction.new('NORTH', [ 0,  1]),
+        Direction.new('EAST',  [ 1,  0]),
+        Direction.new('SOUTH', [ 0, -1]),
+        Direction.new('WEST',  [-1,  0]),
       ]
     end
 
     def coordinates
-      [0, 0]
+      super || [0, 0]
     end
 
+    def make_changes(directive)
+    end
   end
 end
